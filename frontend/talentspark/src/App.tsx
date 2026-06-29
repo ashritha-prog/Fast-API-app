fadb8533f5e69185c43f1c7be030e0dbf1a5b197
ashritha-prog <ashritha21105@gmail.com>
2026-06-29 22:17:03 +1100

    Frontend component

---
+++ frontend/talentspark/src/App.tsx
@@ -0,0 +1,17 @@
+import Welcome from './components/Welcome';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import CompanyCard from './components/CompanyCard';
+import JobCard from './components/JobCard';
+function App(){
+    return(
+      <div>
+        <NavBar />
+        <Welcome/>
+        <CompanyCard/>
+        <JobCard/>
+        <Footer/>
+      </div>
+    )
+}
+export default App;
---
+++ frontend/test.js
@@ -0,0 +1,42 @@
+// console.log("Hello World");
+// let x=10;
+// console.log(x);
+// const y=20;
+// console.log(y);
+// console.log(x+y);
+
+// function add(x,y){
+//     return x+y
+// }
+// console.log(add(10,20));
+// console.log(add[0]);
+// console.log(add[1]);
+// console.log(add[2]);
+// console.log(add[3]);
+// console.log(add[4]);
+
+// const dict={"name":"john","age":30,"city":"New York"};
+// console.log(dict.name);
+
+
+// let {name,age,city} = dict;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// let a=[10,20];
+// let b=[30,40];
+// console.log(...a,...b);
+// console.log(...a,...b);
+
+// function ass(a,...b){
+//     return a+b;
+
+// }
+// console.log(add(10,20));
+// console.log(add(10,20,30));
+// console.log(add(10,20,30,40));
+
+const a=10;
+const b=20;
+console.log(`The sum os ${a} and ${b} is ${a+b}`);
